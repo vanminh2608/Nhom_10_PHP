5918f90ca4d1e62009f248c3e506382fe737b5a2
Van Minh <101303669+vanminh2608@users.noreply.github.com>
2024-04-19 06:51:30 +0000

    my_EX

--- index.php
+++ index.php
@@ -5,23 +5,31 @@
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *---------------------------------------------------------------------------------------*/
 
-function sayHello($name) {
+function sayHello($name)
+{
 	echo "Hello $name!";
 }
 
 ?>
 
 <html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
+
+<head>
+	<title>Visual Studio Code Remote :: PHP</title>
+</head>
+
+<body>
+	<?php
+	printf("<p>I love you</p>")
+	?>
+	<?php
+	$num = 5;
+	$string = "Quan ngu";
+	printf("num = %d va string = %s\n", $num, $string);
+	echo $num, " va ", $string;
+	$num1 = 6;
+	echo $sum = $num + $num1;
+	?>
+</body>
+
 </html>
