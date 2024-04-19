e0eb9f2cdf301ca8def3e3939679c4555ec009a6
Van Minh <101303669+vanminh2608@users.noreply.github.com>
2024-04-19 07:11:59 +0000

    else_if

--- index.php
+++ index.php
@@ -26,11 +26,13 @@ function sayHello($name)
 	$num = 5;
 	$string = "Quan ngu";
 	printf("num = %d va string = %s", $num, $string);
-	echo $num, " va ", $string , "\n";
+	echo $num, " va ", $string, "\n";
 	$num1 = 6;
 	echo $sum = $num + $num1, "\n";
 	?>
-
+	<?php
+	printf("<p>--------------</p>")
+	?>
 	<?php
 
 	$arr = array(1, 2, 3);
@@ -38,6 +40,23 @@ function sayHello($name)
 		echo $value, "\n";
 	}
 	?>
+	<?php
+	printf("<p>--------------</p>")
+	?>
+
+	<?php 
+	$soluong = 11;
+	$gia = 200000;
+	if($soluong < 10)
+		$thanh_tien = $soluong * $gia;
+	elseif($soluong >= 10 and $soluong <=20)
+		$thanh_tien = ($soluong * $gia) * 0.95;
+	else 
+		$thanh_tien = $soluong * $gia;
+
+		echo $thanh_tien;
+	?>
+
 </body>
 
 </html>
